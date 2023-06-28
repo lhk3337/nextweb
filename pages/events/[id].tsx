270760f4413e7271913ad66d3aefb08240e77ba4
lhk3337 <lhk3337@naver.com>
2023-06-28 17:17:05 +0900

    [FEAT] add event detail page

--- pages/events/[id].tsx
+++ pages/events/[id].tsx
@@ -1,10 +1,27 @@
 import React from "react";
 import { useRouter } from "next/router";
+import { getEventById } from "../../data/dummy-data";
+import { ItemType } from "../../types/types";
+import DateIcon from "../../components/icons/date-icon";
+import AddressIcon from "../../components/icons/address-icon";
+import Image from "next/image";
 
 const EventDetail = () => {
   const router = useRouter();
-  console.log(router.query);
-  return <div>id{router.query.id}</div>;
+  const id = router.query.id as string;
+  const events = getEventById(id) as ItemType;
+
+  return (
+    <div>
+      <h1>{events?.title}</h1>
+      <Image src={`/${events?.image}`} alt={events?.id} width={400} height={400} priority />
+      <DateIcon />
+      <div>{events?.date}</div>
+      <AddressIcon />
+      <div>{events?.location}</div>
+      <span>{events?.description}</span>
+    </div>
+  );
 };
 
 export default EventDetail;
