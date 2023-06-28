e8373d0b83a90f1e850dc35770acb5e8b36b79b7
lhk3337 <lhk3337@naver.com>
2023-06-28 17:20:40 +0900

    [FIX] modify event item componet

--- components/item.tsx
+++ components/item.tsx
@@ -1,13 +1,23 @@
 import React from "react";
 import { ItemType } from "../types/types";
-import Link from "next/link";
+import clasess from "./item.module.css";
+import Image from "next/image";
+import Button from "./button";
+import ArrowRightIcon from "../components/icons/arrow-right-icon";
 
-const Item = ({ title, description, location, date, image, id }: ItemType) => {
+const Item = ({ title, location, date, image, id }: ItemType) => {
   return (
     <li>
+      <Image src={`/${image}`} alt={id} width={250} height={250} />
       <span>{title}</span>
-      <span>{description}</span>
-      <Link href={`/events/${id}`}>이동</Link>
+      <span>{date}</span>
+      <span>{location}</span>
+      <Button link={`/events/${id}`}>
+        Explore Event
+        <span className={clasess.icon}>
+          <ArrowRightIcon />
+        </span>
+      </Button>
     </li>
   );
 };
