c181ebdc9e3df479b04fbcdee5733561619f1559
lhk3337 <lhk3337@naver.com>
2023-06-29 17:39:10 +0900

    [FEAT] Item cart css styling

--- components/item.tsx
+++ components/item.tsx
@@ -1,23 +1,44 @@
 import React from "react";
 import { ItemType } from "../types/types";
-import clasess from "./item.module.css";
+import classes from "./item.module.css";
 import Image from "next/image";
 import Button from "./button";
 import ArrowRightIcon from "../components/icons/arrow-right-icon";
+import DateIcon from "./icons/date-icon";
+import AddressIcon from "./icons/address-icon";
 
 const Item = ({ title, location, date, image, id }: ItemType) => {
+  const fetchedDate = new Date(date).toLocaleDateString("en-US", { month: "long", year: "numeric", day: "numeric" });
+  const fetchedLocation = location.trim().split(", ");
+
   return (
-    <li>
-      <Image src={`/${image}`} alt={id} width={250} height={250} />
-      <span>{title}</span>
-      <span>{date}</span>
-      <span>{location}</span>
-      <Button link={`/events/${id}`}>
-        Explore Event
-        <span className={clasess.icon}>
-          <ArrowRightIcon />
-        </span>
-      </Button>
+    <li className={classes.item}>
+      <Image src={`/${image}`} alt={id} width="0" height="0" sizes="100vw" className={classes.thumbnail} />
+      <div className={classes.content}>
+        <div className={classes.summary}>
+          <h2>{title}</h2>
+          <div className={classes.date}>
+            <DateIcon />
+            <time>{fetchedDate}</time>
+          </div>
+          <div className={classes.address}>
+            <AddressIcon />
+            <div>
+              {fetchedLocation.map((value, index) => (
+                <address key={index}>{value}</address>
+              ))}
+            </div>
+          </div>
+        </div>
+        <div className={classes.actions}>
+          <Button link={`/events/${id}`}>
+            Explore Event
+            <span className={classes.icon}>
+              <ArrowRightIcon />
+            </span>
+          </Button>
+        </div>
+      </div>
     </li>
   );
 };
