72fa1cd378127ef4dd6ad57652cd27aaae20e00d
lhk3337 <lhk3337@naver.com>
2023-07-07 18:27:22 +0900

    [FEAT] password decoding

--- libs/auth.ts
+++ libs/auth.ts
@@ -1,5 +1,10 @@
-import { hash } from "bcryptjs";
+import { compare, hash } from "bcryptjs";
 export async function hashPassword(password: string) {
   const hashedPassword = await hash(password, 12);
   return hashedPassword;
+}
+
+export async function verifyPassword(password: string, hashedPassword: string) {
+  const isValid = await compare(password, hashedPassword);
+  return isValid;
 }
